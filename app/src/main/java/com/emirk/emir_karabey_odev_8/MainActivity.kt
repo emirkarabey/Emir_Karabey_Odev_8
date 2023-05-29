c17c30bbf29270d8d42aba6936510c08b5b174af
Emirkrby <63194364+emirkarabey@users.noreply.github.com>
2023-05-29 21:14:17 +0300

    Add android entry point

--- app/src/main/java/com/emirk/emir_karabey_odev_8/MainActivity.kt
+++ app/src/main/java/com/emirk/emir_karabey_odev_8/MainActivity.kt
@@ -1,18 +1,18 @@
 package com.emirk.emir_karabey_odev_8
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
 import com.emirk.emir_karabey_odev_8.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration

2672b2f040c4a589b126afc2976ca5bb3e66c5d3
Emirkrby <63194364+emirkarabey@users.noreply.github.com>
2023-05-29 21:42:51 +0300

    Add Spinner

--- app/src/main/java/com/emirk/emir_karabey_odev_8/ui/add_person/AddPersonFragment.kt
+++ app/src/main/java/com/emirk/emir_karabey_odev_8/ui/add_person/AddPersonFragment.kt
@@ -1,12 +1,16 @@
 package com.emirk.emir_karabey_odev_8.ui.add_person
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.emirk.emir_karabey_odev_8.R
 import com.emirk.emir_karabey_odev_8.databinding.FragmentAddPersonBinding
 import com.emirk.emir_karabey_odev_8.domain.ui_model.Person
 import dagger.hilt.android.AndroidEntryPoint
@@ -18,7 +22,7 @@ class AddPersonFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val viewModel: AddPersonViewModel by viewModels()
-
+    private var selectedGroup: String? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -29,16 +33,39 @@ class AddPersonFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        setupGroupSpinner()
         binding.btnAdd.setOnClickListener {
             val person = Person(
-                groupName = binding.etGroupName.text.toString(),
+                groupName = selectedGroup.toString(),
                 personName = binding.etPersonName.text.toString(),
                 personPhoneNo = binding.etPhoneNo.text.toString(),
                 personAddress = binding.etAddress.text.toString()
             )
             viewModel.addPerson(person)
             findNavController().navigate(AddPersonFragmentDirections.actionAddPersonFragmentToNavHome())
+        }
+    }
+
+    @SuppressLint("ResourceType")
+    private fun setupGroupSpinner() {
+        val adapter = ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.spinner_items,
+            android.R.layout.simple_spinner_item
+        )
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        binding.spinnerGroup.adapter = adapter
+
+        binding.spinnerGroup.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                selectedGroup = parent.getItemAtPosition(position).toString()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                selectedGroup = "Aile"
+            }
         }
     }
 }

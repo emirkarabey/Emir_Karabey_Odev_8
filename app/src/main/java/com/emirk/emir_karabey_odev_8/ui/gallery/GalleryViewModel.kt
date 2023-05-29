e1b69bc0a57d43fe31fcb5991c95fe34ad5c50b4
Emirkrby <63194364+emirkarabey@users.noreply.github.com>
2023-05-29 17:59:40 +0300

    Initial commit

---
+++ app/src/main/java/com/emirk/emir_karabey_odev_8/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.emirk.emir_karabey_odev_8.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}

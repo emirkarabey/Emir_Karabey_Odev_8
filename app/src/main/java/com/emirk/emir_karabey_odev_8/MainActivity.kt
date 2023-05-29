6d16dcf071b93ced07dff533454bdfd07fa82f7c
Emirkrby <63194364+emirkarabey@users.noreply.github.com>
2023-05-29 22:11:40 +0300

    Add navigation

--- app/src/main/java/com/emirk/emir_karabey_odev_8/MainActivity.kt
+++ app/src/main/java/com/emirk/emir_karabey_odev_8/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.emirk.emir_karabey_odev_8
+
 import android.os.Bundle
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -16,13 +18,13 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
-private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-     binding = ActivityMainBinding.inflate(layoutInflater)
-     setContentView(binding.root)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
@@ -31,22 +33,49 @@ private lateinit var binding: ActivityMainBinding
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.nav_home), drawerLayout)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home
+            ), drawerLayout
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
         binding.navView.setNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
+                R.id.nav_hepsi -> {
+                    navigateToHomeFragmentWithParameter("Hepsi")
+                    true
+                }
                 R.id.nav_aile -> {
+                    navigateToHomeFragmentWithParameter("Aile")
+                    true
+                }
+                R.id.nav_okul -> {
+                    navigateToHomeFragmentWithParameter("Okul")
                     true
                 }
                 R.id.nav_arkadaslar -> {
+                    navigateToHomeFragmentWithParameter("Arkadaşlar")
+                    true
+                }
+                R.id.nav_hali_saha -> {
+                    navigateToHomeFragmentWithParameter("Halı Saha")
+                    true
+                }
+                R.id.nav_is -> {
+                    navigateToHomeFragmentWithParameter("İş")
                     true
                 }
                 else -> false
             }
         }
+    }
+
+    private fun navigateToHomeFragmentWithParameter(parameter: String) {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        val bundle = bundleOf("parameter" to parameter)
+        navController.navigate(R.id.nav_home, bundle)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {

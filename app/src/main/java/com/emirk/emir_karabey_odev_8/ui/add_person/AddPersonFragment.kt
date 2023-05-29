3c04b1ea7d2f18bf19cf896b370d6e8d0c154f92
Emirkrby <63194364+emirkarabey@users.noreply.github.com>
2023-05-29 21:14:57 +0300

    Get and add person

--- app/src/main/java/com/emirk/emir_karabey_odev_8/ui/add_person/AddPersonFragment.kt
+++ app/src/main/java/com/emirk/emir_karabey_odev_8/ui/add_person/AddPersonFragment.kt
@@ -1,19 +1,44 @@
 package com.emirk.emir_karabey_odev_8.ui.add_person
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.emirk.emir_karabey_odev_8.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.emirk.emir_karabey_odev_8.databinding.FragmentAddPersonBinding
+import com.emirk.emir_karabey_odev_8.domain.ui_model.Person
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class AddPersonFragment : Fragment() {
+
+    private var _binding: FragmentAddPersonBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: AddPersonViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_person, container, false)
+    ): View {
+        _binding = FragmentAddPersonBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnAdd.setOnClickListener {
+            val person = Person(
+                groupName = binding.etGroupName.text.toString(),
+                personName = binding.etPersonName.text.toString(),
+                personPhoneNo = binding.etPhoneNo.text.toString(),
+                personAddress = binding.etAddress.text.toString()
+            )
+            viewModel.addPerson(person)
+            findNavController().navigate(AddPersonFragmentDirections.actionAddPersonFragmentToNavHome())
+        }
     }
 }
--- app/src/main/java/com/emirk/emir_karabey_odev_8/ui/home/HomeFragment.kt
+++ app/src/main/java/com/emirk/emir_karabey_odev_8/ui/home/HomeFragment.kt
@@ -5,13 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.emirk.emir_karabey_odev_8.databinding.FragmentHomeBinding
+import com.emirk.emir_karabey_odev_8.domain.ui_model.Person
+import com.emirk.emir_karabey_odev_8.ui.home.adapter.PersonAdapter
+import com.emirk.emir_karabey_odev_8.ui.home.adapter.PersonItemClickListener
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: HomeViewModel by viewModels()
+    private lateinit var adapter: PersonAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,10 +34,51 @@ class HomeFragment : Fragment() {
     ): View {
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.getFavorites()
+        initRecyclerViewAdapters()
+        collectEvent()
         binding.fab.setOnClickListener {
             findNavController().navigate(HomeFragmentDirections.actionNavHomeToAddPersonFragment())
         }
-        return binding.root
+    }
+
+    private fun initRecyclerViewAdapters() {
+        adapter = PersonAdapter(object : PersonItemClickListener {
+            override fun onItemClick(person: Person) {
+                TODO("Not yet implemented")
+            }
+        })
+        setupRecyclerViews()
+    }
+
+    private fun setupRecyclerViews() = with(binding) {
+        rvPerson.layoutManager = GridLayoutManager(context, 1)
+        rvPerson.adapter = adapter
+    }
+
+    private fun collectEvent() = binding.apply {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.collect { uiState ->
+                    if (uiState.isLoading) {
+                        progressBar.visibility = View.VISIBLE
+                    } else {
+                        progressBar.visibility = View.INVISIBLE
+                        adapter.submitList(uiState.person)
+                    }
+
+                    uiState.userMessage?.let {
+                        binding.tvError.text = it
+                        viewModel.userMessageShown()
+                    }
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {

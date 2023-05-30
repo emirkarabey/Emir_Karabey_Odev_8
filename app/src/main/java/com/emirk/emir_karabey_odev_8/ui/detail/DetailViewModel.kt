96561aee2907229fd7f1f1e529fa42efe35684b1
Emirkrby <63194364+emirkarabey@users.noreply.github.com>
2023-05-30 19:48:30 +0300

    Add delete person function in view model

--- app/src/main/java/com/emirk/emir_karabey_odev_8/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/emirk/emir_karabey_odev_8/ui/detail/DetailViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.emirk.emir_karabey_odev_8.common.Resource
 import com.emirk.emir_karabey_odev_8.data.local.entity.PersonEntity
+import com.emirk.emir_karabey_odev_8.domain.use_case.DeletePersonUseCase
 import com.emirk.emir_karabey_odev_8.domain.use_case.GetPersonDetailUseCase
 import com.emirk.emir_karabey_odev_8.domain.use_case.UpdatePersonUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -18,7 +19,8 @@ import javax.inject.Inject
 @HiltViewModel
 class DetailViewModel @Inject constructor(
     private val getPersonDetailUseCase: GetPersonDetailUseCase,
-    private val updatePersonUseCase: UpdatePersonUseCase
+    private val updatePersonUseCase: UpdatePersonUseCase,
+    private val deletePersonUseCase: DeletePersonUseCase
 ) : ViewModel() {
 
     private val _uiState = MutableStateFlow(PersonDetailUiState())
@@ -48,5 +50,9 @@ class DetailViewModel @Inject constructor(
 
     fun updatePerson(personEntity: PersonEntity) = viewModelScope.launch {
         updatePersonUseCase.invoke(personEntity)
+    }
+
+    fun deletePerson(personId: Int) = viewModelScope.launch {
+        deletePersonUseCase.invoke(personId)
     }
 }

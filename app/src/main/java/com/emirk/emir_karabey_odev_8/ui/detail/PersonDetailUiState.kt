9093ae4c2b29d8c5de05a0154239638d57abc626
Emirkrby <63194364+emirkarabey@users.noreply.github.com>
2023-05-30 19:34:25 +0300

    Change ui state

--- app/src/main/java/com/emirk/emir_karabey_odev_8/ui/detail/PersonDetailUiState.kt
+++ app/src/main/java/com/emirk/emir_karabey_odev_8/ui/detail/PersonDetailUiState.kt
@@ -1,9 +1,9 @@
 package com.emirk.emir_karabey_odev_8.ui.detail
 
-import com.emirk.emir_karabey_odev_8.domain.ui_model.Person
+import com.emirk.emir_karabey_odev_8.data.local.entity.PersonEntity
 
 data class PersonDetailUiState(
     val isLoading: Boolean = false,
     val userMessage: String? = null,
-    val person: Person? = null
+    val person: PersonEntity? = null
 )
